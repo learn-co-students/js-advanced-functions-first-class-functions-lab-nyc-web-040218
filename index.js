dd1835a2a87b9800ab3e2372a0d0ff314b7f75e8
briecodes <hello@brie.work>
2018-05-16 20:19:26 -0400

    lab completed.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(arr) {
+    return arr.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(arr) {
+    return arr.slice(arr.length - 2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+    return function(fare){
+        return fare * int;
+    }
+}
+
+const fareDoubler = function(fare) {
+    return createFareMultiplier(fare)(2);
+}
+
+const fareTripler = function (fare) {
+    return createFareMultiplier(fare)(3);
+}
+
+function selectDifferentDrivers(drivers, fn) {
+    return fn(drivers);
+}
